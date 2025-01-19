37c23e03df922ad49aeb78502c0452f33683c3c0
Pietro Borges Parri <83304020+PietroParri@users.noreply.github.com>
2025-01-19 03:49:49 -0300

    mega haste update

--- data/spells/scripts/support/Mega Haste.lua
+++ data/spells/scripts/support/Mega Haste.lua
@@ -1,2 +1,12 @@
-local combat = createCombatObject()setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)local condition = createConditionObject(CONDITION_HASTE)setConditionParam(condition, CONDITION_PARAM_TICKS, 66000)setConditionFormula(condition, 1.7, -36, 1.7, -36)setCombatCondition(combat, condition)function onCastSpell(cid, var)        return doCombat(cid, combat, var)end
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 
+local condition = createConditionObject(CONDITION_HASTE)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 66000)
+setConditionFormula(condition, 1.7, -60, 1.7, -60)
+setCombatCondition(combat, condition)
+
+function onCastSpell(cid, var)
+    return doCombat(cid, combat, var)
+end
